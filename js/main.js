3cf6ac8fefd5fe98a3f51a1f76540a7f63f67a13
Andrei Margineanu <a.margineanu@UNIFUN.LOCAL>
2018-04-10 13:54:11 +0300

    add main functionality for geocode

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-const geocode = () => {
-  const location = '22 Main st Boston MA';
+const geocode = (e) => {
+  e.preventDefault();
+  const location = document.getElementById('location-input').value;
   axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
     params: {
       address: location,
@@ -13,11 +14,33 @@ const geocode = () => {
         <li class="list-group-item">${formatedAddress}</li>
       </ul>
     `;
+    const addressComponents = response.data.results[0].address_components;
+
+    let addressComponentsOutput = '<ul class="list-group">'
+
+    addressComponents.forEach((item, index) => {
+      addressComponentsOutput += `<li class="list-group-item"><strong>${addressComponents[index].types[0]}</strong>: ${addressComponents[index].long_name}</li>`;
+    })
+
+    addressComponentsOutput += '</ul>'
+
+    const lat = response.data.results[0].geometry.location.lat;
+    const lng = response.data.results[0].geometry.location.lat;
+    const geometryOutput = `
+      <ul class="list-group">
+        <li class="list-group-item"><strong>Latitude:</strong> ${lat}</li>
+        <li class="list-group-item"><strong>Longitude:</strong> ${lng}</li>
+      </ul>
+    `;
+
     document.getElementById("formatted-address").innerHTML = formattedAdressOutput;
+    document.getElementById("address-components").innerHTML = addressComponentsOutput;
+    document.getElementById("geometry").innerHTML = geometryOutput;
   })
   .catch((error) => {
     console.log(error);
   });
 }
 
-geocode();
+const locationForm = document.getElementById('location-form');
+locationForm.addEventListener('submit', geocode);
